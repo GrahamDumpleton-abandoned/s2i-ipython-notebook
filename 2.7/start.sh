775221a734db92b2e4d059759a7d5d42d20b44b1
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-11-30 12:34:40 +1100

    Parameterise hostname for ipcontroller.

--- 2.7/start.sh
+++ 2.7/start.sh
@@ -2,11 +2,20 @@
 
 cp -rp /.whiskey/ipython $HOME/.ipython
 
-if [ "$IPYTHON_CONTAINER_TYPE" = "controller" ]; then
+if [ x"$IPYTHON_CLUSTER_LABEL" != x"" ]; then
+    cat /.whiskey/ipython/security/ipcontroller-client.json | \
+        sed -e "s/ipcontroller/ipcontroller-$IPYTHON_CLUSTER_LABEL/" > \
+        $HOME/.ipython/profile_default/security/ipcontroller-client.json
+    cat /.whiskey/ipython/security/ipcontroller-engine.json | \
+        sed -e "s/ipcontroller/ipcontroller-$IPYTHON_CLUSTER_LABEL/" > \
+        $HOME/.ipython/profile_default/security/ipcontroller-engine.json
+fi
+
+if [ x"$IPYTHON_CONTAINER_TYPE" = x"controller" ]; then
     exec ipcontroller
 fi
 
-if [ "$IPYTHON_CONTAINER_TYPE" = "engine" ]; then
+if [ x"$IPYTHON_CONTAINER_TYPE" = x"engine" ]; then
     exec ipengine
 fi
 
