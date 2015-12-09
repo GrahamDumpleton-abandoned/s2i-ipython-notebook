a61eff68a7896e3483f5875d57f07bbb6398943a
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-12-09 11:33:20 +1100

    Update for new user/permissions structure.

--- 2.7/start.sh
+++ 2.7/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-cp -rp /.whiskey/jupyter $HOME/.jupyter
-cp -rp /.whiskey/ipython $HOME/.ipython
+cp -rp $HOME/jupyter $HOME/.jupyter
+cp -rp $HOME/ipython $HOME/.ipython
 
 if [ x"$IPYTHON_CLUSTER_LABEL" != x"" ]; then
-    cat /.whiskey/ipython/profile_default/security/ipcontroller-client.json | \
+    cat $HOME/ipython/profile_default/security/ipcontroller-client.json | \
         sed -e "s/ipcontroller/ipcontroller-$IPYTHON_CLUSTER_LABEL/" > \
         $HOME/.ipython/profile_default/security/ipcontroller-client.json
-    cat /.whiskey/ipython/profile_default/security/ipcontroller-engine.json | \
+    cat $HOME/ipython/profile_default/security/ipcontroller-engine.json | \
         sed -e "s/ipcontroller/ipcontroller-$IPYTHON_CLUSTER_LABEL/" > \
         $HOME/.ipython/profile_default/security/ipcontroller-engine.json
 fi
